6a00b4007bf8e3b14ec48415716fa7b79c6d0976
scottchou1018 <scottchou1018@gmail.com>
2024-12-23 16:40:49 +0800

    update(user): add create, findOne, findID service

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService){}
+
+    @Post()
+    createUser(@Body() createUserDto: Prisma.UserCreateInput){
+
+        return this.userService.create(createUserDto);
+    }
+
+}
