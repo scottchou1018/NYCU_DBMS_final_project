4f413baef8d8a5667b769b038ddc9b4df4dc0bbc
scottchou1018 <scottchou1018@gmail.com>
2024-12-25 10:18:22 +0800

    update(contest): finish contest api

--- backend/src/contest/contest.service.ts
+++ backend/src/contest/contest.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class ContestService {}
+export class ContestService {
+    constructor(private readonly databaseService: DatabaseService){}
+    
+
+    async getContest(contestId: number){
+        const contest = await this.databaseService.contest.findUnique({
+            where:{
+                contestId: contestId
+            }
+        })
+        if(!contest){
+            throw new BadRequestException('the contestId not found')
+        }
+        return contest
+    }
+
+    /**
+     * if the contest haven't existed in database, then it will create one
+     * then it will return the contest data in the database
+     * @param contestId the contest id in codeforces
+     * @param contestName the name of the contest
+     */
+    async checkOrCreateContest(contestId: number, contestName: string){
+        let contest = await this.databaseService.contest.findUnique({
+            where: {
+                contestId: contestId
+            }
+        })
+        if(!contest){
+            contest = await this.databaseService.contest.create({
+                data:{
+                    contestId: contestId,
+                    contestName: contestName
+                }
+            })
+        }
+        return contest
+    }
+}
