bb7714331eb0bbacf14168ba05b94b50dbc27780
scottchou1018 <scottchou1018@gmail.com>
2024-12-25 14:35:59 +0800

    update(scoreboard): finish scoreboard query api

--- backend/src/scoreboard/scoreboard.controller.ts
+++ backend/src/scoreboard/scoreboard.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { LoginedGuard } from 'src/auth/utils/guards/LoginedGuard';
+import { ScoreboardService } from './scoreboard.service';
 
 @Controller('scoreboard')
-export class ScoreboardController {}
+export class ScoreboardController {
+    constructor(private readonly scoreboardService: ScoreboardService){}
+
+    @UseGuards(LoginedGuard)
+    @Get()
+    getScoreboardOfGroup(@Req() req, @Query('groupId') groupId: string, @Query('contestId') contestId: string){
+        return this.scoreboardService.getScoreboardOfGroup(req.user.userId, +groupId, +contestId)
+    }
+}
