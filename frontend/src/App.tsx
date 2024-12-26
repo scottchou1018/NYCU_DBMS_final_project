2b02ff0c18d1cb98b0ab88660bd7e12b92272b63
yyh_603 <yiyanghung603@gmail.com>
2024-12-26 15:46:20 +0800

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './assets/Home';
+import About from './assets/About';
+import NotFound from './assets/NotFound';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
