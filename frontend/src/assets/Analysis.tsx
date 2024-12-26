cc32c7834a8f25735f18a9500f456e0afd1d87b5
yyh_603 <yiyanghung603@gmail.com>
2024-12-26 19:30:39 +0800

    update(frontend/home): add frontend auth

--- frontend/src/assets/Analysis.tsx
+++ frontend/src/assets/Analysis.tsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
 function Analysis() {
+  const [userId, setUser] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchUserStatus = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/auth/status',{
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          withCredentials: true
+        });
+        console.log(response)
+        setUser(response.data.userId);
+      } catch (error) {
+        console.error('There was an error fetching the user status!', error);
+      }
+    };
+
+    fetchUserStatus();
+  }, []);
     return (
       <div>
         <h1>Analysis Page</h1>
-        <p>Welcome to the Analysis page!</p>
+        <p>Welcome { userId ? userId : 'Guest' } to the Analysis page!</p>
       </div>
     );
 }
