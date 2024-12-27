1618b157e5d3973e10f323270ede43a210ea1311
scottchou1018 <scottchou1018@gmail.com>
2024-12-27 18:20:34 +0800

    update(backend/group): add patch method to group

--- backend/src/group/dto/createGroupDto.ts
+++ backend/src/group/dto/createGroupDto.ts
@@ -1,4 +1,5 @@
-import { IsArray, IsInt, IsNotEmpty, IsString, ArrayMinSize } from "class-validator";
+import { empty } from "@prisma/client/runtime/library";
+import { IsArray, IsInt, IsNotEmpty, IsString, ArrayMinSize, IsOptional } from "class-validator";
 
 
 export class CreateGroupDto{
@@ -11,4 +12,17 @@ export class CreateGroupDto{
     @IsInt({each: true})
     @ArrayMinSize(1)
     public readonly teams: number[];
+}
+
+export class UpdateGroupDto{
+
+    @IsOptional()
+    @IsString()
+    public readonly groupName?: string;
+
+    @IsOptional()
+    @IsArray()
+    @IsInt({each: true})
+    @ArrayMinSize(1)
+    public readonly teams?: number[];
 }
